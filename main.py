94a3e9a37566db053dad424f1fdc6ddf09fbdbe6
vr63 <vikram6jayanth3@gmail.com>
2023-01-09 13:18:40 +0530

    Added features : 1. Added a berry image 2. Povided a feature that plots the berry in random locations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 import math
+import random
 
 # Making a camera object
 
@@ -13,13 +14,21 @@ cap.set(4, 720)
 detector = HandDetector(detectionCon = .8, maxHands = 1)
 
 class SnakeGameClass:
-    def __init__(self) -> None:
+    def __init__(self, path : str) -> None:
         self.points = [] # All points in the snake
         self.lengths = [] # The length of all points in the snake
         self.currentLength = 0 # Total length of the snake
-        self.allowedLength = 150
+        self.allowedLength = 130
         self.previousHead = 0, 0
-    
+
+        self.imgFood = cv.imread(path, cv.IMREAD_UNCHANGED)
+        self.hFood, self.wFood, _ = self.imgFood.shape
+        self.foodPoint = 0, 0
+        self.randomFoodLocation()
+    def randomFoodLocation(self) -> int:
+        self.foodPoint = random.randint(100, 1100), random.randint(100,620)
+
+
     def update(self, imgMain, currentHead) -> None:
         px, py = self.previousHead
         cx, cy = currentHead
@@ -29,15 +38,31 @@ class SnakeGameClass:
         self.currentLength += distance
         self.previousHead = currentHead
 
-        # Drawing Snake
+        # Length Reduction
+        if self.currentLength > self.allowedLength:
+            for i, length in enumerate(self.lengths):
+                self.currentLength -= length
+                self.points.pop(i)
+                self.lengths.pop(i)
+                if self.currentLength < self.allowedLength:
+                    break
+            
 
-        for i, point in enumerate(self.points):
-            if i != 0:
-                cv.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
-        cv.circle(imgMain, self.points[-1], 20, (200, 0, 200), cv.FILLED)
+
+        # Drawing Snake
+        if self.points:
+            for i, point in enumerate(self.points):
+                if i != 0:
+                    cv.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+            cv.circle(imgMain, self.points[-1], 20, (200, 0, 200), cv.FILLED)
+
+
+        # Draw the food
+        rx, ry = self.foodPoint
+        imgMain = cvzone.overlayPNG(imgMain, self.imgFood, (rx - (self.wFood // 2), ry - (self.hFood // 2)))
         return imgMain        
 
-game = SnakeGameClass()
+game = SnakeGameClass("berry.png")
 
 while True:
     ret, img = cap.read()
